ed81098728db268252d912103938801d15993768
WasinUddy <38974240+WasinUddy@users.noreply.github.com>
2020-12-24 15:57:50 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'banana-log',         # How you named your package folder (MyLib)
-  packages = ['banana-log'], # Chose the same as "name"
+  name = 'banana_log',         # How you named your package folder (MyLib)
+  packages = [], # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Make printing Program Log easy with fancy color',   # Give a short description about your library
